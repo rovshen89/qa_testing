52485fee1e1c7f2010ad1464069c250367bf2586
rovshen89 <rovshen89@gmail.com>
2020-05-14 13:54:30 -0500

    Delta_Test

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,40 @@
+import okhttp3.internal.NamedRunnable;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.sql.DriverPropertyInfo;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "D:\\QA_Testing\\Resources\\chromedriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        driver.quit();
+        driver.get("https://www.delta.com/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("fromAirportName")).click();
+        driver.findElement(By.id("search_input")).sendKeys("NYC");
+
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        String script = "return document.getElementById(\"search_input\").value;";
+        String text = (String) js.executeScript(script);
+        System.out.println(text);
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("search_input")).sendKeys(Keys.ENTER);
+        System.out.println(driver.findElement(By.xpath("//span[@class='airport-desc']")).getText());
+        driver.findElement(By.id("toAirportName")).click();
+        driver.findElement((By.id("search_input"))).sendKeys("ASB");
+        Thread.sleep(2000);
+        driver.findElement((By.id("search_input"))).sendKeys(Keys.ENTER);
+
+
+
+
+
+        //        driver.quit();
 
     }
 }
